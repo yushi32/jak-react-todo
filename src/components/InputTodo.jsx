01a707b7c36b3e074b6eb794caff101d3dbd116b
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-10-02 15:22:16 +0000

    update: CSS-in-JSのチュートリアルとして、テキストボックスのCSSをインライン表記に変更

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: '#c1ffff',
+  width: '400px',
+  height: '30px',
+  padding: '8px',
+  margin: '8px',
+  borderRadius: '8px'
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-      <div className="input-area">
+      <div style={style}>
       <input
         placeholder="TODOを入力"
        value={todoText}
