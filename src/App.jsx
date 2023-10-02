f473ad573e11cbd55f0d20e96b0a0245a2f98dab
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-10-02 14:13:45 +0000

    update: 未完了リストをコンポーネントファイルに分割

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
-import {InputTodo} from "./components/InputTodo";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
 
 export const App = () => {
   const [todoText, setTodoText] = useState('');
@@ -55,22 +56,11 @@ export const App = () => {
       onChange={onChangeTodoText}
       onClick={onClickAdd}
     />
-    <div className="incomplete-area">
-      <p className="title">未完了のTODO</p>
-      <ul>
-        {incompleteTodos.map((todo, index) => {
-          return (
-            <li key={todo}>
-              <div className="list-row">
-                <p>{todo}</p>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <IncompleteTodos
+      todos={incompleteTodos}
+      onClickComplete={onClickComplete}
+      onClickDelete={onClickDelete}
+    />
     <div className="complete-area">
     <p className="title">未完了のTODO</p>
       <ul>
